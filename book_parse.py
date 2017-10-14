5937770afa146e3c40362c188469d1134501a846
Murtaza <Murch786@gmail.com>
2017-10-14 14:23:05 -0400

    File Path Relative
    Made the file path relative instead of absolute for extraction

--- book_parse.py
+++ book_parse.py
@@ -1,15 +1,17 @@
 import zipfile
+import os
 from bs4 import BeautifulSoup
 
 word_dictionary = {}
+curr_dir = os.getcwd()
 
 # def book_parse(fname):
 
 def extract_epub(fname):
     with zipfile.ZipFile(fname, "r") as myzip:
-        myzip.extractall("/Users/murtazaHusain/Desktop/Murtaza/Georgia Tech/HackGT4/Extracted Files")
+        myzip.extractall(os.path.join(curr_dir, "Extracted Files"))
 
-# extract_epub("[Linda_Nagata]_The_Bohr_Maker(BookZZ.org).epub")
+extract_epub("[Linda_Nagata]_The_Bohr_Maker(BookZZ.org).epub")
 
 def extract_xhtml(fname):
     with open(fname) as html_fp:
@@ -18,7 +20,7 @@ def extract_xhtml(fname):
     raw_text = chapter.get_text()
     return raw_text
 
-raw_text = extract_xhtml("index_split_002.xhtml")
+# raw_text = extract_xhtml("index_split_002.xhtml")
 
 def text_to_dict(text):
     words = text.split()
