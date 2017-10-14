a9aba6623089849a03b5d833ce3b49e4322e8e84
Murtaza <Murch786@gmail.com>
2017-10-14 16:49:19 -0400

    book_parse fully functional
    book_parse now returns the full text of the input book as an array of each word

--- book_parse.py
+++ book_parse.py
@@ -5,31 +5,29 @@ from bs4 import BeautifulSoup
 word_dictionary = {}
 curr_dir = os.getcwd()
 
-<<<<<<< HEAD
 def book_parse(fname):
     print(fname)
     book_text = []
     base_fname = os.path.splitext(fname)[0]
     # creates a folder with name of book
     extract_epub(fname, base_fname)
-    chapter_list = get_fnames(base_fname, ".xhtml")
+    chapter_list = get_fnames(base_fname, ".xhtml")[0:-1]
 
-
+    os.chdir(os.path.join(curr_dir, base_fname))
     for chapter in chapter_list:
         chapter_text = extract_xhtml(chapter)
+        chapter_text = chapter_text.split()
         book_text = book_text + chapter_text
+    os.chdir(curr_dir)
 
     return book_text
-=======
-# def book_parse(fname):
->>>>>>> parent of 87548a0... book_parse
 
 def extract_epub(fname, end_dir):
     with zipfile.ZipFile(fname, "r") as myzip:
         myzip.extractall(os.path.join(curr_dir, end_dir))
 
 def extract_xhtml(fname):
-    with open(os.path.join(/Users/murtazaHusain/Documents/GitHub/BabelSort/[Linda_Nagata]_The_Bohr_Maker(BookZZ.org), fname)) as html_fp:
+    with open(fname) as html_fp:
         chapter = BeautifulSoup(html_fp, "html.parser")
 
     raw_text = chapter.get_text()
@@ -53,11 +51,6 @@ def get_fnames(directory, ftype):
     return ftype_list
 
 # print(get_fnames("Extracted Files", ".xhtml"))
-<<<<<<< HEAD
 # book_list = get_fnames("", ".epub")
 # print(book_list)
 print(book_parse("[Linda_Nagata]_The_Bohr_Maker(BookZZ.org).epub"))
-=======
-book_list = get_fnames("", ".epub")
-print(book_list)
->>>>>>> parent of 87548a0... book_parse
