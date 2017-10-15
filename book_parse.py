d3c3ab1d5d964563e879fc9d1fff62ebd0166db4
Murtaza <Murch786@gmail.com>
2017-10-14 20:39:08 -0400

    Clean up files

--- book_parse.py
+++ book_parse.py
@@ -6,30 +6,30 @@ import epub_conversion
 from epub_conversion.utils import open_book, convert_epub_to_lines
 from bs4 import BeautifulSoup
 
-def book_parse(fname):
-    book = open_book(fname)
-    lines = convert_epub_to_lines(book)
-    lines = ''.join(lines)
-    lines = remove_punc(cleanhtml(lines))
-    lines = lines.split()
-    return lines
+class BookParser(object):
+    def __init__(self):
 
-def cleanhtml(raw_html):
-  cleanr = re.compile('<.*?>')
-  cleantext = re.sub(cleanr, '', raw_html)
-  return cleantext
+    def book_parse(fname):
+        book = open_book(fname)
+        lines = convert_epub_to_lines(book)
+        lines = ''.join(lines)
+        lines = remove_punc(cleanhtml(lines))
+        lines = lines.split()
+        return lines
 
-def remove_punc(text):
-    exclude = set(string.punctuation)
-    text = ''.join(char for char in text if char not in exclude)
-    return text
+    def cleanhtml(raw_html):
+      cleanr = re.compile('<.*?>')
+      cleantext = re.sub(cleanr, '', raw_html)
+      return cleantext
 
-def get_fnames(directory, ftype):
-    ftype_list = []
-    for file in os.listdir(os.path.join(curr_dir, directory)):
-        if file.endswith(ftype):
-            ftype_list.append(file)
-    return ftype_list
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        text = ''.join(char for char in text if char not in exclude)
+        return text
 
-
-print(book_parse("[Linda_Nagata]_The_Bohr_Maker(BookZZ.org).epub"))
+    def get_fnames(directory, ftype):
+        ftype_list = []
+        for file in os.listdir(os.path.join(curr_dir, directory)):
+            if file.endswith(ftype):
+                ftype_list.append(file)
+        return ftype_list
