4ac627df7e92fe2999244463f1f31f94f7377cf3
Unknown <lasia.lo@duke.edu>
2017-10-15 04:37:18 -0400

    Didn't push for a while lmao

--- src/Learn_Simple.py
+++ src/Learn_Simple.py
@@ -23,8 +23,7 @@ class Learn(object):
         #Graph input
         #parameters
         CHUNKLENGTH = 250
-        NUMBEROFGENRES = 2
-        
+        NUMBEROFGENRES = 5
         trainTexts = np.array(trainTexts)
         trainGenres = np.array(trainGenres)
         testTexts = np.array(testTexts)
@@ -34,11 +33,11 @@ class Learn(object):
         nodeWeight = tf.Variable(tf.zeros([CHUNKLENGTH,NUMBEROFGENRES])) 
         nodeBias = tf.Variable(tf.zeros([NUMBEROFGENRES]))
 
-        
         # y = x * W + b
         actualOutcome = (tf.matmul(inputSample,nodeWeight) + nodeBias)
         
         expectedOutcome = tf.placeholder(tf.float32, [None, NUMBEROFGENRES])
+        
         
         crossEntropy = tf.reduce_mean(
             tf.nn.softmax_cross_entropy_with_logits(labels=expectedOutcome,logits=actualOutcome))
@@ -49,13 +48,13 @@ class Learn(object):
         tf.global_variables_initializer().run()
         
         # Train
-        for step in range(1000):
+        for step in range(5000):
             sampleBatch = [trainTexts[step]]
             genreBatch = [trainGenres[step]]
-            if (step % 100 == 0):
-                    correctPrediction = tf.equal(tf.argmax(actualOutcome,1), tf.argmax(expectedOutcome,1))
-                    accuracy = tf.reduce_mean(tf.cast(correctPrediction, tf.float32))
-                    print(session.run(accuracy, feed_dict={inputSample: testTexts,expectedOutcome: testGenres}))
+#             if (step%200 == 0):
+#                 correctPrediction = tf.equal(tf.argmax(actualOutcome,1), tf.argmax(expectedOutcome,1))
+#                 accuracy = tf.reduce_mean(tf.cast(correctPrediction, tf.float32))
+#                 print(session.run(accuracy, feed_dict={inputSample: testTexts,expectedOutcome: testGenres}))
             session.run(trainingStep, feed_dict={inputSample: sampleBatch,expectedOutcome:genreBatch})
         
       
@@ -63,5 +62,7 @@ class Learn(object):
         # Testing
         correctPrediction = tf.equal(tf.argmax(actualOutcome,1), tf.argmax(expectedOutcome,1))
         accuracy = tf.reduce_mean(tf.cast(correctPrediction, tf.float32))
+        print(session.run(nodeWeight))
+        print(session.run(nodeBias))
         print(session.run(accuracy, feed_dict={inputSample: testTexts,expectedOutcome: testGenres}))
         
--- src/Main.py
+++ src/Main.py
@@ -6,11 +6,12 @@ Created on Oct 14, 2017
 import Learn_Simple
 import book_parse
 import tf_glove
-import numpy
+import numpy as np
 
 parser = book_parse.BookParser()
+
+#fantasy
 fileNames = parser.get_fnames('Data Sets\Fantasy','.epub')
- 
 lines = parser.book_parse(fileNames[0])
 lines = parser.list_parser(lines, 1000)
 lines = lines[0:50]
@@ -19,20 +20,110 @@ model = tf_glove.GloVeModel(embedding_size=250,context_size=4)
 model.fit_to_corpus(lines)
 model.train(num_epochs=5)
 trainInput = model.embeddings
-trainInput = trainInput[0:3000]
-trainGenre = [[0,1]] * 3000
-# 
+trainInput = trainInput[0:1000]
 
-#  
-lines2 = parser.book_parse(fileNames[1])
-lines2 = parser.list_parser(lines2, 1000)
-lines2 = lines2[0:50]
-model2 = tf_glove.GloVeModel(embedding_size=250,context_size=2)
-model2.fit_to_corpus(lines2)
+
+trainGenre = [[1,0,0,0,0]] * 1000
+
+print(len(trainInput))
+print("2")
+fileNames = parser.get_fnames('Data Sets\History','.epub')
+lines = parser.book_parse(fileNames[0])
+lines = parser.list_parser(lines, 1000)
+lines = lines[0:50]
+ 
+model = tf_glove.GloVeModel(embedding_size=250,context_size=4)
+model.fit_to_corpus(lines)
+model.train(num_epochs=5)
+trainInput2 = model.embeddings
+trainInput2 = trainInput2[0:1000]
+trainInput = np.append([trainInput] , [trainInput2],axis=1)
+trainInput = trainInput[0]
+trainGenre = trainGenre+[[0,1,0,0,0]] * 1000
+print(len(trainInput))
+
+print("3")
+fileNames = parser.get_fnames('Data Sets\Literature','.epub')
+lines = parser.book_parse(fileNames[0])
+lines = parser.list_parser(lines, 1000)
+lines = lines[0:50]
+ 
+model = tf_glove.GloVeModel(embedding_size=250,context_size=4)
+model.fit_to_corpus(lines)
+model.train(num_epochs=5)
+trainInput2 = model.embeddings
+trainInput2 = trainInput2[0:1000]
+
+trainInput = np.append([trainInput] , [trainInput2],axis=1)
+trainInput = trainInput[0]
+
+trainGenre = trainGenre+[[0,0,1,0,0]] * 1000
+
+print(len(trainInput))
+print("4")
+fileNames = parser.get_fnames('Data Sets\Science','.epub')
+lines = parser.book_parse(fileNames[0])
+lines = parser.list_parser(lines, 1000)
+lines = lines[0:50]
+ 
+model = tf_glove.GloVeModel(embedding_size=250,context_size=4)
+model.fit_to_corpus(lines)
+model.train(num_epochs=5)
+trainInput2 = model.embeddings
+trainInput2 = trainInput2[0:1000]
+
+trainInput = np.append([trainInput] , [trainInput2],axis=1)
+trainInput = trainInput[0]
+
+trainGenre = trainGenre+[[0,0,0,1,0]] * 1000
+
+print(len(trainInput))
+print("5")
+fileNames = parser.get_fnames('Data Sets\Science Fiction','.epub')
+lines = parser.book_parse(fileNames[0])
+lines = parser.list_parser(lines, 1000)
+lines = lines[0:50]
+ 
+model = tf_glove.GloVeModel(embedding_size=250,context_size=4)
+model.fit_to_corpus(lines)
+model.train(num_epochs=5)
+trainInput2 = model.embeddings
+trainInput2 = trainInput2[0:1000]
+
+trainInput = np.append([trainInput] , [trainInput2],axis=1)
+trainInput = trainInput[0]
+
+trainGenre = trainGenre+[[0,0,0,0,1]] * 1000
+print(len(trainInput))
+print(len(trainGenre))
+
+print("Now Creating Testcase")
+fileNames = parser.get_fnames('Data Sets\History','.epub')
+lines = parser.book_parse(fileNames[2])
+lines = parser.list_parser(lines, 1000)
+lines = lines[0:50]
+ 
+model2 = tf_glove.GloVeModel(embedding_size=250,context_size=4)
+model2.fit_to_corpus(lines)
 model2.train(num_epochs=5)
 testInput = model2.embeddings
-testInput = testInput[0:3000]
-testGenre = [[0,1]] * 3000
+
+testInput = (trainInput[0:1000])  
+testGenre = [[0,1,0,0,0]] * 1000
+
+print(len(testInput))
+print(len(testGenre))
+
+#  
+# lines2 = parser.book_parse(fileNames[1])
+# lines2 = parser.list_parser(lines2, 1000)
+# lines2 = lines2[0:50]
+# model2 = tf_glove.GloVeModel(embedding_size=250,context_size=2)
+# model2.fit_to_corpus(lines2)
+# model2.train(num_epochs=5)
+# testInput = model2.embeddings
+# testInput = testInput[0:1000]
+# testGenre = [[0,1]] * 1000
 print("now starting formal training")
 # trainInput = numpy.array([[0,1,2,3,4],[0,1,4,4,4]])
 # trainGenre = numpy.array([[0,1],[0,1]])
