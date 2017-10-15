0e0e23830f5ed8b15139c18206998cc8ee7a3ee2
Unknown <lasia.lo@duke.edu>
2017-10-14 20:05:54 -0400

    Updating stuff

--- src/Learn_Simple.py
+++ src/Learn_Simple.py
@@ -8,46 +8,58 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+import numpy as np
 
-genres = ['a','b','c','d','e']
-
-#need to get the batches of text
-
-
-#need to find way to embed code into numbers
+class Learn(object):
+    '''
+    Simple neural network - one layer
+    '''
 
 
-#parameters
+    def __init__(self, trainTexts,trainGenres,testTexts,testGenres):
+        '''
+        Constructor
+        '''
+        #Graph input
+        #parameters
+        CHUNKLENGTH = 100
+        NUMBEROFGENRES = 4
+        
+        trainTexts = np.array(trainTexts)
+        trainGenres = np.array(trainGenres)
+        testTexts = np.array(testTexts)
+        testGenres = np.array(testGenres)
+        
+        inputSample = tf.placeholder(tf.float32, [None, CHUNKLENGTH]) #array size of unlimited, each array size is textLength
+        nodeWeight = tf.Variable(tf.zeros([CHUNKLENGTH,NUMBEROFGENRES])) 
+        nodeBias = tf.Variable(tf.zeros([NUMBEROFGENRES]))
 
-chunkLength = 100
-numberOfGenres = 4
-   
-#Graph input
-inputSample = tf.placeholder(tf.float32, [None, chunkLength]) #array size of unlimited, each array size is textLength
-nodeWeight = tf.Variable(tf.zeros([chunkLength,numberOfGenres])) 
-nodeBias = tf.Variable(tf.zeros[10])
+        
+        # y = x * W + b
+        actualOutcome = (tf.matmul(inputSample,nodeWeight) + nodeBias)
+        
+        expectedOutcome = tf.placeholder(tf.float32, [None, NUMBEROFGENRES])
+        
+        crossEntropy = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(labels=expectedOutcome,logits=actualOutcome))
+        
+        trainingStep = tf.train.GradientDescentOptimizer(0.5).minimize(crossEntropy)
+        
+        session = tf.InteractiveSession()
+        tf.global_variables_initializer().run()
+        
+        # Train
+       
+        for step in range(1):
+            sampleBatch = trainTexts[step]
+            genreBatch = trainGenres[step]
+            session.run(trainingStep, feed_dict={inputSample: sampleBatch,expectedOutcome:genreBatch})
+        
+        print("bruh")
 
-# y = x * W + b
-actualOutcome = tf.matmul(inputSample,nodeWeight) + nodeBias
-
-expectedOutcome = tf.placeholder(tf.float32, [None, numberOfGenres])
-
-crossEntropy = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(expectedOutcome,actualOutcome))
-
-trainingStep = tf.train.GradientDescentOptimizer(0.5).minimize(crossEntropy)
-
-session = tf.InteractiveSession()
-tf.global_variables_initializer().run()
-
-# Train
-for _ in range(1000):
-    sampleBatch, genreBatch = 
-    sess.run(trainingStep,{sampleBatch, genreBatch})
-
-# Testing
-correctPrediction = tf.equal(tf.argmax(actualOutcome,1), tf.argmax(expectedOutcome,1))
-accuracy = tf.reduce_mean(tf.cast(correctPrediction, tf.float32))
-
-print(session.run(accuracy,{testinput,testlabels}))
-
+        # Testing
+        correctPrediction = tf.equal(tf.argmax(actualOutcome,1), tf.argmax(expectedOutcome,1))
+        accuracy = tf.reduce_mean(tf.cast(correctPrediction, tf.float32))
+        
+        print(session.run(accuracy, feed_dict={inputSample: testTexts,expectedOutcome: testGenres}))
+        
---
+++ src/Main.py
@@ -0,0 +1,9 @@
+'''
+Created on Oct 14, 2017
+
+@author: lasia lo
+'''
+import Learn_Simple
+
+learn = Learn_Simple.Learn([3,3],[2,2],[3,3],[2,2])
+print("sup")
