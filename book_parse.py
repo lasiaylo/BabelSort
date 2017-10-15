88cd0fdb9b0a88a395051ce9554371e70eab66fd
Murtaza <Murch786@gmail.com>
2017-10-14 20:08:21 -0400

    Redone
    Redid entire file. It can now take an .epub file and converts it to text. It is then put into a list of words

--- book_parse.py
+++ book_parse.py
@@ -1,47 +1,28 @@
 import zipfile
 import os
+import string
+import re
+import epub_conversion
+from epub_conversion.utils import open_book, convert_epub_to_lines
 from bs4 import BeautifulSoup
 
-word_dictionary = {}
-curr_dir = os.getcwd()
-
 def book_parse(fname):
-    print(fname)
-    book_text = []
-    base_fname = os.path.splitext(fname)[0]
-    # creates a folder with name of book
-    extract_epub(fname, base_fname)
-    chapter_list = get_fnames(base_fname, ".xhtml")[0:-1]
+    book = open_book(fname)
+    lines = convert_epub_to_lines(book)
+    lines = ''.join(lines)
+    lines = remove_punc(cleanhtml(lines))
+    lines = lines.split()
+    return lines
 
-    os.chdir(os.path.join(curr_dir, base_fname))
-    for chapter in chapter_list:
-        chapter_text = extract_xhtml(chapter)
-        chapter_text = chapter_text.split()
-        book_text = book_text + chapter_text
-    os.chdir(curr_dir)
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
 
-    return book_text
-
-def extract_epub(fname, end_dir):
-    with zipfile.ZipFile(fname, "r") as myzip:
-        myzip.extractall(os.path.join(curr_dir, end_dir))
-
-def extract_xhtml(fname):
-    with open(fname) as html_fp:
-        chapter = BeautifulSoup(html_fp, "html.parser")
-
-    raw_text = chapter.get_text()
-    return raw_text
-
-# raw_text = extract_xhtml("index_split_002.xhtml")
-
-def text_to_dict(text):
-    words = text.split()
-    for word in words:
-        if word in word_dictionary:
-            word_dictionary[word] = word_dictionary[word] + 1
-        else:
-            word_dictionary[word] = 1
+def remove_punc(text):
+    exclude = set(string.punctuation)
+    text = ''.join(char for char in text if char not in exclude)
+    return text
 
 def get_fnames(directory, ftype):
     ftype_list = []
@@ -50,7 +31,5 @@ def get_fnames(directory, ftype):
             ftype_list.append(file)
     return ftype_list
 
-# print(get_fnames("Extracted Files", ".xhtml"))
-# book_list = get_fnames("", ".epub")
-# print(book_list)
+
 print(book_parse("[Linda_Nagata]_The_Bohr_Maker(BookZZ.org).epub"))
